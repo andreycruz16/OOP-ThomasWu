a5ba782b17e63909b3979f50e55152d1aebaed3f
Mark Andrey Dela Cruz <markandreydelacruz@ymail.com>
2015-04-13 17:09:47 +0800

    RandomExer update

--- src/random/RandomExer.java
+++ src/random/RandomExer.java
@@ -5,6 +5,7 @@ public class RandomExer {
 	public static void main(String[] args) {
 		Random random = new Random();
 		System.out.println(random.nextInt(6 - 5 +1) + 5);
+		System.out.println("Thank You fot using! :)");
 
 	}
 }
